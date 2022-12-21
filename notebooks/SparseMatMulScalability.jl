79871dab5b8eabf78da4536d570a11ef1e51c31d
JordiManyer <j.manyer@hotmail.com>
2022-12-21 15:39:38 +0100

    Added more outputs to the driver

--- notebooks/SparseMatMulScalability.jl
+++ notebooks/SparseMatMulScalability.jl
@@ -15,12 +15,52 @@ begin
 end
 
 # ╔═╡ 7e0f9b1e-8980-4711-af2d-46bca71aac1f
-collect_results(projectdir("test"))
+df = collect_results(projectdir("experiments/SparseMatMulScalability/data"))
 
 # ╔═╡ 723abba6-47ca-4d28-8881-4b325336ee6d
+begin
+	df[!,:used_memory] = df[!,:total_memory] - df[!,:free_memory]
+	sort!(df)
+end
+
+# ╔═╡ e2f5dd1f-ac18-4964-9185-caf18ecdb35f
+begin
+	df[!,:ndofs] = [3145728,12582912,50331648,12582912,50331648,201326592]
+	df[!,:ndofsxproc] = df[!,:ndofs] .÷ df[!,:num_procs]
+end
+
+# ╔═╡ 104d2142-0b9d-42ec-b84d-9225c8b32efc
+begin
+	plt = plot()
+	for ndofs in unique(df[!,:ndofsxproc])
+		dfi = df[df.ndofsxproc .== ndofs,:]
+		sort!(dfi)
+		y = map(x -> x[:max],dfi[!,:time_total]) ./ dfi[!,:num_procs]
+		plot!(plt,dfi[!,:num_procs] .÷ 48,y)
+	end
+	plt
+end
+
+# ╔═╡ 0783858d-7ce3-4bf1-8b42-1275f35b95f8
+begin
+	plt2 = plot()
+	for np in unique(df[!,:num_procs])
+		dfi = df[df.num_procs .== np,:]
+		sort!(dfi)
+		y = map(x -> x[:max],dfi[!,:time_total]) ./ np
+		plot!(plt2,dfi.ndofsxproc,y)
+	end
+	plt2
+end
+
+# ╔═╡ 2e633dcb-ef9f-45aa-af53-6527e6697576
 
 
 # ╔═╡ Cell order:
 # ╠═e42a3f88-7fb8-11ed-2b3a-ed9befa26d9d
 # ╠═7e0f9b1e-8980-4711-af2d-46bca71aac1f
 # ╠═723abba6-47ca-4d28-8881-4b325336ee6d
+# ╠═e2f5dd1f-ac18-4964-9185-caf18ecdb35f
+# ╠═104d2142-0b9d-42ec-b84d-9225c8b32efc
+# ╠═0783858d-7ce3-4bf1-8b42-1275f35b95f8
+# ╠═2e633dcb-ef9f-45aa-af53-6527e6697576
--- src/SparseMatMulDrivers.jl
+++ src/SparseMatMulDrivers.jl
@@ -19,20 +19,34 @@ function sparse_matmul_driver(parts,t,outputs,partition,order)
   y = PVector(0.0,A.rows)
   mul!(y,A,x)
 
+  ndofs    = size(A,1)
+  nnz      = nnz(A)
+
   free_mem = Sys.free_memory()/2^20
   tot_mem  = Sys.total_memory()/2^20
 
+  N = 20
   niter = 200
-  mytic!(t,comm)
-  for i in 1:niter
-    mul!(y,A,x)
+  for k in 1:N
+    mytic!(t,comm)
+    for i in 1:niter
+      mul!(y,A,x)
+    end
+    PartitionedArrays.toc!(t,"time_$k")
   end
-  PartitionedArrays.toc!(t,"time_total")
   
-  if i_am_main(parts)
-    outputs["free_memory"] = free_mem
-    outputs["total_memory"] = tot_mem
+  map_main(t.data) do t_data
+    time = 0.0
+    for k in 1:N
+      time = min(time,t_data[:max])
+    end
+
+    outputs["free_memory"]     = free_mem
+    outputs["total_memory"]    = tot_mem
     outputs["num_repetitions"] = niter
+    outputs["num_dofs"]        = ndofs
+    outputs["num_non_zeros"]   = nnz
+    outputs["curated_time"]    = time
   end
 end
 
@@ -45,7 +59,7 @@ function sparse_matmul_main(;title::AbstractString,np::Tuple,nc::Tuple)
     
     map_main(t.data) do timer_data
       outputs["ARCH"] = Sys.ARCH
-      outputs["CPU"] = Sys.CPU_NAME
+      outputs["CPU"]  = Sys.CPU_NAME
       outputs["num_procs"] = prod(np)
       merge!(outputs,timer_data)
       save("$title.bson",outputs)
